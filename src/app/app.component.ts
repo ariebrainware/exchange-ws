6e89d71883fdc2ba23049b30035f693d74126f07
Rob0ne <rob0ne@localhost.localdomain>
2018-12-10 15:49:26 +0700

    Add Angular basic template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<ng2-smart-table [settings]="settings"></ng2-smart-table>`
 })
 export class AppComponent {
-  title = 'exchange-ws';
+  title = 'exchange-ws'
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      name: {
+        title: 'Full Name'
+      },
+      username: {
+        title: 'User Name'
+      },
+      email: {
+        title: 'Email'
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Ng2SmartTableModule } from 'ng2-smart-table'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng2SmartTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
